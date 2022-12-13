60f719f926f9e1eb7e708efc9242793936b3896d
NgcoboAyanda <ayanda13@protonmail.com>
2022-12-13 12:28:11 +0200

    initialized project

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="app">
+        <div className="app__inner">
+          <div>
+            this is the app dummy
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App;
